a1b1aca08bdbd7f8fa058f6eb7bd4a6412d32edb
Anurag shrestha <anuragshrestha117@gmail.com>
2025-06-01 17:00:50 +0545

    this

--- LSPPDay1/index.js
+++ LSPPDay1/index.js
@@ -1,30 +1,26 @@
 const https = require("https");
 
-function fetchBitcoinPrice() {
+function fetchPrice() {
   const url =
     "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,01coin&vs_currencies=usd";
 
-  https
-    .get(url, (res) => {
-      let data = "";
+  https.get(url, (res) => {
+    let data = "";
 
-      res.on("data", (chunk) => {
-        data += chunk;
-      });
-
-      res.on("end", () => {
-        try {
-          const json = JSON.parse(data);
-          //   const btc = json[0];
-          console.log("Bitcoin Market Data:\n", json);
-        } catch (err) {
-          console.error("Error parsing response:", err.message);
-        }
-      });
-    })
-    .on("error", (err) => {
-      console.error("Request failed:", err.message);
+    res.on("data", (chunk) => {
+      data = chunk + data;
     });
+
+    res.on("end", () => {
+      try {
+        const json = JSON.parse(data);
+
+        console.log("Bitcoin Market Data:\n", json);
+      } catch (err) {
+        console.error("Error parsing response:", err.message);
+      }
+    });
+  });
 }
 
-fetchBitcoinPrice();
+fetchPrice();
