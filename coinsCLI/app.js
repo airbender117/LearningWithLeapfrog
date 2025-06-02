07f279c841e23f46dde9e239de6a7655feb0b133
Anurag shrestha <anuragshrestha117@gmail.com>
2025-06-02 16:14:11 +0545

    executable

--- coinsCLI/app.js
+++ coinsCLI/app.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import axios from "axios";
@@ -18,31 +19,60 @@ const argv = yargs(hideBin(process.argv))
     demandOption: false,
     default: "usd",
   })
+  .option("refresh", {
+    alias: "r",
+    describe: "Refresh interval in seconds",
+    type: "number",
+    default: 10,
+  })
   .help()
   .alias("help", "h").argv;
 const coins = argv.coins.slice("");
 // console.log(coins);
 const currencies = argv.currencies;
 // console.log(currencies);
+const refreshIntervals = argv.refresh * 1000;
 // const args = process.argv.slice(2);
 // const coins = args.length ? args.join(",") : "bitcoin,01coin";
+console.clear();
+console.log("Shitting in my pants...");
+console.log(`Last updated: ${new Date().toLocaleTimeString()}`);
 
-axios
-  .get(
-    `https://api.coingecko.com/api/v3/simple/price?ids=${coins}&vs_currencies=${currencies}`
-  )
-  .then(function (res) {
-    const prices = res.data;
-    // console.log(prices);
+function fetchPrices() {
+  axios
+    .get(
+      `https://api.coingecko.com/api/v3/simple/price?ids=${coins}&vs_currencies=${currencies}`
+    )
+    .then(function (res) {
+      const prices = res.data;
+      console.clear();
+      console.log("Results");
 
-    for (const coin in prices) {
-      console.log(
-        `${coin.toUpperCase()}:  ${prices[coin].usd} USD, ${
-          prices[coin].aud
-        } AUD`
-      );
-    }
-  })
-  .catch(function (err) {
-    console.log("Error fetching data:", err.message);
-  });
+      // console.log(prices);
+
+      for (const coin in prices) {
+        const values = Object.entries(prices[coin])
+          .map(([currency, price]) => `${currency.toUpperCase()}: ${price}`)
+          .join(" | ");
+        // console.log(currency, price);
+        console.log(`${coin.toUpperCase()}:  ${values}`);
+      }
+      console.log(`Last updated: ${new Date().toLocaleTimeString()}`);
+
+      let count = 9;
+
+      const interval = setInterval(() => {
+        process.stdout.write(`\rRefreshes in: ${count}  `);
+        count--;
+
+        if (count < 0) {
+          clearInterval(interval);
+        }
+      }, 1000);
+    })
+    .catch(function (err) {
+      console.log("Error fetching data:", err.message);
+    });
+}
+fetchPrices();
+setInterval(fetchPrices, refreshIntervals);
